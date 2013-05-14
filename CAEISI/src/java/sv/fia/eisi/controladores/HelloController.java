d9c42a213dba1ff9bae80bfbba6363e177913471
jasdbeat@gmail.com <jasdbeat@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-05-14 03:47:37 +0000

    agregados archivos de prueba de funcionamiento ZK + Spring

---
+++ CAEISI/src/java/sv/fia/eisi/controladores/HelloController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.fia.eisi.controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author antonio
+ */
+@Controller
+public class HelloController {
+    
+     protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value="/hello")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning hello view");
+
+        return new ModelAndView("test.zul");
+    }
+}
