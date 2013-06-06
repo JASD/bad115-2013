4f5feb988925cf7b01bc458375448fef5ac29d35
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-06 23:27:38 +0000

    drkld

--- trunk/CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
@@ -1,7 +1,32 @@
 package sv.fia.eisi.controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author Mario Sanchez
  */
+@Controller
 public class ActividadController {
-    
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value = "/nuevaActividad")
+    public ModelAndView newActivity(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Retuning nuevaActividad view");
+        return new ModelAndView("nueva_actividad.zul");
+    }
+
+    @RequestMapping(value = "/actividades")
+    public ModelAndView allActivities(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Retuning actividades view");
+        return new ModelAndView("actividades.zul");
+    }
 }
