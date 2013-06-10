d9d195abec1a26b7596fb269bdcbc8192260f412
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-10 01:19:17 +0000

    drkl

--- trunk/CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
@@ -9,24 +9,35 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.Composer;
+import org.zkoss.zul.Grid;
 
 /**
  * @author Mario Sanchez
  */
 @Controller
-public class ActividadController {
+public class ActividadController implements Composer<Component>{
 
     protected final Log logger = LogFactory.getLog(getClass());
 
+    private Grid activityList;
+    
     @RequestMapping(value = "/nuevaActividad")
     public ModelAndView newActivity(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         logger.info("Retuning nuevaActividad view");
-        return new ModelAndView("nueva_actividad.zul");
+        return new ModelAndView("actividad/nueva_actividad");
     }
 
     @RequestMapping(value = "/actividades")
     public ModelAndView allActivities(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         logger.info("Retuning actividades view");
-        return new ModelAndView("actividades.zul");
+        
+        return new ModelAndView("actividad/actividad");
+    }
+
+    @Override
+    public void doAfterCompose(Component t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
