378e6b4ac78675b6850cea8aef1183539b481d4f
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-11 22:11:11 +0000

    drkl

---
+++ trunk/CAEISI/src/java/sv/fia/eisi/repositorios/ActividadDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.fia.eisi.repositorios;
+
+/**
+ *
+ * @author admin
+ */
+public class ActividadDAO {
+    
+}
