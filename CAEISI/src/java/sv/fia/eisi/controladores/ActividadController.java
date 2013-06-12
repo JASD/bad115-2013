1e8bc99c7373783e6d20b6493033140915c6bc5f
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-12 13:29:22 +0000

    drkl

--- CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
+++ CAEISI/src/java/sv/fia/eisi/controladores/ActividadController.java
@@ -1,13 +1,17 @@
 package sv.fia.eisi.controladores;
 
+import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.ListModelList;
+import sv.fia.eisi.repositorios.ActividadDAO;
 
 /**
  * @author Mario Sanchez
  */
+@Controller
 public class ActividadController extends SelectorComposer<Component> {
 
     @Wire
@@ -16,5 +20,7 @@ public class ActividadController extends SelectorComposer<Component> {
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
+        activityList=(Grid) comp;
+        activityList.setModel(new ListModelList<ActividadDAO>());
     }
 }
