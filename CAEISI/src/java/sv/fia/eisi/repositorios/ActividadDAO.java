29dff27ea75a0dbf3b63ba352652183b2acf7e35
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-12 14:10:05 +0000

    drkl

--- CAEISI/src/java/sv/fia/eisi/repositorios/ActividadDAO.java
+++ CAEISI/src/java/sv/fia/eisi/repositorios/ActividadDAO.java
@@ -4,10 +4,37 @@
  */
 package sv.fia.eisi.repositorios;
 
+import org.springframework.stereotype.Repository;
+import sv.fia.eisi.entidades.Actividad;
+
 /**
  *
  * @author admin
  */
-public class ActividadDAO {
-    
+@Repository
+public class ActividadDAO extends AbstractDAO<Actividad> {
+
+    public ActividadDAO() {
+        super(Actividad.class);
+    }
+
+    @Override
+    public void create(Actividad entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit(Actividad entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Actividad entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Actividad find(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
