2912e58ad3366c8f6324d6fcf8e22a7ffd99c32a
jasdbeat@gmail.com <jasdbeat@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-13 04:05:54 +0000

    cambio de import inncesario

--- trunk/CAEISI/src/java/sv/fia/eisi/repositorios/CursoDAO.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/repositorios/CursoDAO.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import sv.fia.eisi.entidades.Curso;
 
 /**
