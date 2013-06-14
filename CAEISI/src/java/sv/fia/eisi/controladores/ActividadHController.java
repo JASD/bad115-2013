bf0df012064f3501e1855a4d586f8954649bd8c0
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-14 00:53:16 +0000

    Actividad

---
+++ CAEISI/src/java/sv/fia/eisi/controladores/ActividadHController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.fia.eisi.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class ActividadHController extends SelectorComposer<Component> {
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+    }
+}
