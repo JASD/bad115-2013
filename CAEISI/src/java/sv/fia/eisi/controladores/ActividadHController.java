1ec57b1a16dc74ddd1bcbf419279cec018d21750
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-15 11:58:27 +0000

    Actividad

--- CAEISI/src/java/sv/fia/eisi/controladores/ActividadHController.java
+++ CAEISI/src/java/sv/fia/eisi/controladores/ActividadHController.java
@@ -7,6 +7,14 @@ package sv.fia.eisi.controladores;
 import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Spinner;
+import org.zkoss.zul.Textbox;
+import sv.fia.eisi.entidades.Actividad;
+import sv.fia.eisi.servicios.ActividadService;
 
 /**
  *
@@ -15,8 +23,39 @@ import org.zkoss.zk.ui.select.SelectorComposer;
 @Controller
 public class ActividadHController extends SelectorComposer<Component> {
 
+    @Wire
+    private Textbox codActivity;
+    @Wire
+    private Textbox nomActivity;
+    @Wire
+    private Spinner horasActivity;
+    @Wire
+    private Textbox tipoActivity;
+    @WireVariable
+    private ActividadService actividadService;
+
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
+    }
+
+    @Listen("onClick=#guardarActividad")
+    public void guardarActividad() {
+        Actividad actividad = new Actividad();
+        actividad.setCodigoActividad(codActivity.getValue());
+        actividad.setNombreActividad(nomActivity.getValue());
+        actividad.setNumeroHoras(horasActivity.getValue().shortValue());
+        actividad.setTipoActividad(tipoActivity.getValue());
+        String message = null;
+        String type = null;
+        try {
+            message = actividadService.guardarActividad(actividad);
+            type = Clients.NOTIFICATION_TYPE_INFO;
+        } catch (Exception e) {
+            message = e.getMessage();
+            type = Clients.NOTIFICATION_TYPE_ERROR;
+        } finally {
+            Clients.showNotification(message, type, this.getSelf(), "top_center", 2000, true);
+        }
     }
 }
