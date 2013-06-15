995464d6655629c52dd707727df7d6f21094345c
evercanales@gmail.com <evercanales@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-15 16:26:46 +0000

    modificar ciclo

--- trunk/CAEISI/src/java/sv/fia/eisi/controladores/CicloController.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/controladores/CicloController.java
@@ -4,25 +4,21 @@
  */
 package sv.fia.eisi.controladores;
 
+import java.util.Date;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zk.ui.util.Clients;
-import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
-import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Spinner;
 import org.zkoss.zul.Textbox;
 import sv.fia.eisi.entidades.Ciclo;
 import sv.fia.eisi.entidades.CicloPK;
-import sv.fia.eisi.entidades.Curso;
-import sv.fia.eisi.entidades.Grupo;
-import sv.fia.eisi.entidades.GrupoAcademico;
 import sv.fia.eisi.servicios.CicloService;
-import sv.fia.eisi.servicios.GrupoAcademicoService;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  *
@@ -40,26 +36,38 @@ public class CicloController extends SelectorComposer<Component> {
     private Datebox fechafin;
     @WireVariable
     private CicloService cicloService;
-    
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
+        String fechaUltimaFin = cicloService.findUltimoFechaf().replace("[", "").replace("]", "");
+        String fechaUltimaIn = cicloService.findUltimoFechai().replace("[", "").replace("]", "");
         anyo.setValue(cicloService.findUltimo().replace("[", "").replace("]", ""));
         numero.setValue(cicloService.findUltimoN().replace("[", "").replace("]", ""));
+
+        if (!(fechaUltimaIn.equals("null"))) {
+            fechaini.setValue(getDate(fechaUltimaIn));
+        }
+
+        if (!(fechaUltimaFin.equals("null"))) {
+            fechafin.setValue(getDate(fechaUltimaFin));
+        }
+
+
     }
 
     @Listen("onClick=#guardarCicloAcad")
     public void guardarGrupoAcademico() {
         Ciclo guardar = new Ciclo();
-        CicloPK guardarpk=new CicloPK();
-       
+        CicloPK guardarpk = new CicloPK();
+        guardar.setCicloPK(guardarpk);
+
         guardarpk.setAnoCiclo(Long.parseLong(anyo.getValue()));
         guardarpk.setNumeroCiclo(Short.parseShort(numero.getValue()));
         guardar.setFechaInicioCiclo(fechaini.getValue());
         guardar.setFechaFinCiclo(fechafin.getValue());
-                
-       
+
+
         String message = null;
         String type = null;
         try {
@@ -72,5 +80,18 @@ public class CicloController extends SelectorComposer<Component> {
             Clients.showNotification(message,
                     type, this.getSelf(), "top_center", 2000, true);
         }
+    }
+
+    public Date getDate(String date) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+
+            return df.parse(date);
+
+        } catch (ParseException ex) {
+        }
+
+        return null;
     }
 }
