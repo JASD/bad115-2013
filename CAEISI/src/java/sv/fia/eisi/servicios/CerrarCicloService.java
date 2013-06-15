6b5c4fe4f9a817e0480d9a42946167bb168de598
evercanales@gmail.com <evercanales@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-15 16:28:23 +0000

    cerrar ciclo

---
+++ CAEISI/src/java/sv/fia/eisi/servicios/CerrarCicloService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.fia.eisi.servicios;
+
+import java.util.ResourceBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sv.fia.eisi.repositorios.CerrarCicloDAO;
+
+/**
+ *
+ * @author Ever
+ */
+@Service
+public class CerrarCicloService {
+
+    @Autowired
+    private CerrarCicloDAO cerrarCicloDAO;
+
+    @Transactional
+    public String cerrarCicloAcademico() throws Exception {
+        String status = cerrarCicloDAO.cerrar();
+        if (status.equals("OK")) {
+            return ResourceBundle.getBundle("/messages")
+                    .getString("CicloAcademicoCerrado");
+        } else {
+            throw new Exception(status);
+        }
+    }
+}
