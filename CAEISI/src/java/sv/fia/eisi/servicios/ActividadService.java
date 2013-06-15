472519f13b27d8bd50d9a82cf1b0d53ed8f6b47b
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-15 11:59:01 +0000

    Actividad

--- CAEISI/src/java/sv/fia/eisi/servicios/ActividadService.java
+++ CAEISI/src/java/sv/fia/eisi/servicios/ActividadService.java
@@ -5,6 +5,7 @@
 package sv.fia.eisi.servicios;
 
 import java.util.List;
+import java.util.ResourceBundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +18,22 @@ import sv.fia.eisi.repositorios.ActividadDAO;
  */
 @Service
 public class ActividadService {
+
     @Autowired
     private ActividadDAO actividadDAO;
-    
+
     @Transactional(readOnly = true)
     public List<Actividad> obtenerActividades() {
         return actividadDAO.getActivities();
+    }
+
+    @Transactional
+    public String guardarActividad(Actividad actividad) throws Exception {
+        String status = actividadDAO.create(actividad);
+        if (status.equals("OK")) {
+            return ResourceBundle.getBundle("/messages").getString("ActividadGuardada");
+        } else {
+            throw new Exception(status);
+        }
     }
 }
