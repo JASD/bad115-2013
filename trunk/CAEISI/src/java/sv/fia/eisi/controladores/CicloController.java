3f10184ac71c2ebd5e39fd02ee583bc49d926f5e
evercanales@gmail.com <evercanales@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-15 05:37:53 +0000

    modificar ciclo

---
+++ trunk/CAEISI/src/java/sv/fia/eisi/controladores/CicloController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.fia.eisi.controladores;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Datebox;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Spinner;
+import org.zkoss.zul.Textbox;
+import sv.fia.eisi.entidades.Ciclo;
+import sv.fia.eisi.entidades.CicloPK;
+import sv.fia.eisi.entidades.Curso;
+import sv.fia.eisi.entidades.Grupo;
+import sv.fia.eisi.entidades.GrupoAcademico;
+import sv.fia.eisi.servicios.CicloService;
+import sv.fia.eisi.servicios.GrupoAcademicoService;
+
+/**
+ *
+ * @author Ever
+ */
+public class CicloController extends SelectorComposer<Component> {
+
+    @Wire
+    private Textbox anyo;
+    @Wire
+    private Textbox numero;
+    @Wire
+    private Datebox fechaini;
+    @Wire
+    private Datebox fechafin;
+    @WireVariable
+    private CicloService cicloService;
+    
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        anyo.setValue(cicloService.findUltimo().replace("[", "").replace("]", ""));
+        numero.setValue(cicloService.findUltimoN().replace("[", "").replace("]", ""));
+    }
+
+    @Listen("onClick=#guardarCicloAcad")
+    public void guardarGrupoAcademico() {
+        Ciclo guardar = new Ciclo();
+        CicloPK guardarpk=new CicloPK();
+       
+        guardarpk.setAnoCiclo(Long.parseLong(anyo.getValue()));
+        guardarpk.setNumeroCiclo(Short.parseShort(numero.getValue()));
+        guardar.setFechaInicioCiclo(fechaini.getValue());
+        guardar.setFechaFinCiclo(fechafin.getValue());
+                
+       
+        String message = null;
+        String type = null;
+        try {
+            message = cicloService.actualizarCicloAcademico(guardar);
+            type = Clients.NOTIFICATION_TYPE_INFO;
+        } catch (Exception ex) {
+            message = ex.getMessage();
+            type = Clients.NOTIFICATION_TYPE_ERROR;
+        } finally {
+            Clients.showNotification(message,
+                    type, this.getSelf(), "top_center", 2000, true);
+        }
+    }
+}
