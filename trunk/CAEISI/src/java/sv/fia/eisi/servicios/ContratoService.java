4e1c2eada43c32ef7deca7a1b615acb6a28390ff
mariosanchezsv87@gmail.com <mariosanchezsv87@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-16 18:00:37 +0000

    docente

--- trunk/CAEISI/src/java/sv/fia/eisi/servicios/ContratoService.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/servicios/ContratoService.java
@@ -4,17 +4,21 @@
  */
 package sv.fia.eisi.servicios;
 
-import java.util.Collection;
+import java.util.List;
+import org.springframework.stereotype.Service;
 import sv.fia.eisi.entidades.Contrato;
 
 /**
  *
  * @author Adolfo
  */
+@Service
 public class ContratoService {
 
-    public Collection<? extends Contrato> findActives() {
+    public List<Contrato> findActives() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+   
     
 }
