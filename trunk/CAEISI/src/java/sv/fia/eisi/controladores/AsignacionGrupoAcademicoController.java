0d7d892fb9bd207b599650c6698f45439677c7e8
jasdbeat@gmail.com <jasdbeat@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-16 21:38:08 +0000

    terminada interfaz de asignacion de carga academica

--- trunk/CAEISI/src/java/sv/fia/eisi/controladores/AsignacionGrupoAcademicoController.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/controladores/AsignacionGrupoAcademicoController.java
@@ -6,11 +6,14 @@ package sv.fia.eisi.controladores;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.ListModelList;
 import sv.fia.eisi.entidades.Actividad;
+import sv.fia.eisi.entidades.AsignacionGrupoPK;
 import sv.fia.eisi.entidades.EmpleadoDocente;
 import sv.fia.eisi.entidades.GrupoAcademico;
 import sv.fia.eisi.servicios.ActividadService;
@@ -46,6 +49,27 @@ public class AsignacionGrupoAcademicoController extends SelectorComposer<Compone
         grupos.setModel(new ListModelList<GrupoAcademico>(
                 grupoAcademicoService.obtenerGruposActivos()));
     }
-    
-    
+
+    @Listen("onClick=#guardarAsig")
+    public void asignarCarga() {
+        AsignacionGrupoPK pk = new AsignacionGrupoPK();
+        GrupoAcademico ga = (GrupoAcademico) grupos.getSelectedItem().getValue();
+        Actividad a = (Actividad) actividades.getSelectedItem().getValue();
+        EmpleadoDocente ed = (EmpleadoDocente) docentes.getSelectedItem().getValue();
+        pk.setCodigoGrupo(ga.getCodigoGrupo());
+        pk.setIsssEmpleado(ed.getIsssEmpleado());
+        pk.setCodigoActividad(a.getCodigoActividad());
+        String message = null;
+        String type = null;
+        try {
+            message = grupoAcademicoService.asignarCarga(pk);
+            type = Clients.NOTIFICATION_TYPE_INFO;
+        } catch (Exception ex) {
+            message = ex.getMessage();
+            type = Clients.NOTIFICATION_TYPE_ERROR;
+        } finally {
+            Clients.showNotification(message,
+                    type, this.getSelf(), "top_center", 2000, true);
+        }
+    }
 }
