aa1126f6e73fd0a6ca24e359443ba8b966573211
jasdbeat@gmail.com <jasdbeat@gmail.com@c9a28622-4214-9bf8-bb76-32fec9b1304f>
2013-06-16 22:35:56 +0000

    terminada interfaz de asignacion de coordinacion de cursos

--- trunk/CAEISI/src/java/sv/fia/eisi/repositorios/CursoDAO.java
+++ trunk/CAEISI/src/java/sv/fia/eisi/repositorios/CursoDAO.java
@@ -5,9 +5,12 @@
 package sv.fia.eisi.repositorios;
 
 import java.util.List;
+import java.util.ResourceBundle;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import sv.fia.eisi.entidades.CoordinacionCursoPK;
 import sv.fia.eisi.entidades.Curso;
 
 /**
@@ -43,9 +46,20 @@ public class CursoDAO extends AbstractDAO<Curso> {
     public Curso find(Object id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     public List<Curso> findActives() {
         return (List<Curso>) sessionFactory.getCurrentSession()
                 .getNamedQuery("Curso.findActives").list();
+    }
+
+    public String asignar(CoordinacionCursoPK cc) {
+        String call = ResourceBundle.getBundle("/procedures")
+                .getString("AsignarCoordinacionCurso");
+        Query q = sessionFactory.getCurrentSession()
+                .createSQLQuery(call);
+        q.setString("curso", cc.getCodigoCurso());
+        q.setString("actividad", cc.getCodigoActividad());
+        q.setString("docente", cc.getIsssEmpleado());
+        return (String) q.uniqueResult();
     }
 }
